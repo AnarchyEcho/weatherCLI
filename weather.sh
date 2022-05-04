d49b047b1545073bdf48076a49ebbe971f283c6c
kodeandre <andre.kodehode@gmail.com>
2022-05-04 19:30:31 +0200

    added help flag to bash script

--- weather.sh
+++ weather.sh
@@ -1,5 +1,14 @@
 #!/bin/bash
 function weather {
-    echo `curl --request GET -H Content-Type:application/json https://weatherdbi.herokuapp.com/data/weather/$1 -s` | jq '{ City: .region, Temperature: .currentConditions.temp.c, Time: .currentConditions.dayhour }'
+  echo `curl --request GET -H Content-Type:application/json https://weatherdbi.herokuapp.com/data/weather/$1 -s` | jq '{ City: .region, Temperature: .currentConditions.temp.c, Time: .currentConditions.dayhour }'
   }
-weather $1
+function Help {
+  echo "Incorrect syntax, do: weather <city>"
+  echo "Available flags: -h, --help |"
+}
+case $1 in
+  "" | -h | --help)
+    Help
+    exit;;
+esac
+weather $1 $2
